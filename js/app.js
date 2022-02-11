d2f37b950cf150997d318ce90c4929a0bc5e7fd8
Hazzim-Escarcega <said-hazzim.escarcega@atos.net>
2022-02-11 13:05:24 -0700

    Video 5

--- js/app.js
+++ js/app.js
@@ -8,5 +8,10 @@ console.log(books)
 //finds and selects all the elements within the query
 books = document.querySelectorAll("#book-list li .name")
 Array.from(books).forEach(function(book){
-    console.log(book)
+    console.log(book.textContent)
+    book.textContent += "{Book Title}"
 }) 
+
+const bookList = document.querySelector("#book-list");
+//bookList.innerHTML = '<h2>Books and more books</h2>'
+bookList.innerHTML += '<p>This is how you add HTML...</p>'
