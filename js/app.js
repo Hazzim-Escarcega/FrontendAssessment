cdbc35922ab2310849477d613fa3a64cdb6a03bb
Hazzim-Escarcega <said-hazzim.escarcega@atos.net>
2022-02-11 21:36:45 -0700

    Video 12 Creating Elements

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const list = document.querySelector('#book-list ul');
+const forms = document.forms;
 
 // delete books
 list.addEventListener('click', (e) => {
@@ -7,18 +8,25 @@ list.addEventListener('click', (e) => {
     li.parentNode.removeChild(li);
   }
 });
-//form interaction
-const forms = document.forms;
-console.log(forms);
-console.log(forms['add-book']);
 
-Array.from(forms).forEach(function(form){
-  console.log(form);
-});
-
+// adding books
 const addForm = forms['add-book'];
 addForm.addEventListener('submit', function(e){
   e.preventDefault();
+
+  // creating elements
   const value = addForm.querySelector('input[type="text"]').value;
-  console.log(value);
+  const li = document.createElement('li');
+  const bookName = document.createElement('span');
+  const deleteBtn = document.createElement('span');
+
+  // adding text content
+  bookName.textContent = value;
+  deleteBtn.textContent = 'delete';
+
+  // append to DOM
+  li.appendChild(bookName);
+  li.appendChild(deleteBtn);
+  list.appendChild(li);
+  //list.insertBefore(li, list.querySelector('li:first-child'));
 });
