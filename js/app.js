0d093e0d9a74cae2740c5d181ce14d4c9f856f9a
Hazzim-Escarcega <said-hazzim.escarcega@atos.net>
2022-02-11 21:16:21 -0700

    Video 9 Removing elements

--- js/app.js
+++ js/app.js
@@ -39,3 +39,26 @@ console.log('book-list next previous sibling is: ', bookList.previousSibling)
 console.log('book-list next previous Element sibling is: ', bookList.previousElementSibling)
 
 bookList.previousElementSibling.querySelector('p').innerHTML += '<br/> Too cool for everyone else'
+
+//Add and remove content
+const listItems = document.querySelectorAll('#book-list ul li');
+
+Array.from(listItems).forEach(function(item){
+  item.addEventListener('click', (e) => {
+
+    const li = e.target.parentElement;
+    console.log('child element to remove:', li);
+    console.log('parent element to remove child from:', li.parentElement);
+    li.parentNode.removeChild(li);
+
+  });
+});
+
+// prevent default behaviour
+
+const link = document.querySelector('#page-banner a');
+
+link.addEventListener('click', function(e){
+  e.preventDefault();
+  console.log('Navigation to', e.target.textContent, 'was prevented');
+});
